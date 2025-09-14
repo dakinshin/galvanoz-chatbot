d214a8e4d1d104bbdc617ecbf39f4a2e7904de16
Dmitriy Akinshin <dakinshin@hotmail.com>
2025-09-14 23:49:57 +0300

    pdf.js integration

--- src/app/report/[id]/page.tsx
+++ src/app/report/[id]/page.tsx
@@ -5,7 +5,7 @@ import { load } from "@/lib/submit"
 import { useQuery } from '@tanstack/react-query'
 import { Flex, Space, Spin } from "antd"
 import { useEffect, useState } from "react"
-// import { getDocument, GlobalWorkerOptions } from 'pdfjs-dist'
+import { getDocument, GlobalWorkerOptions } from 'pdfjs-dist'
 
 export default function Page(params: { id: number }) {
   const [url, setUrl] = useState<string>()
@@ -33,49 +33,49 @@ export default function Page(params: { id: number }) {
       console.log('retreivedUrl', retreivedUrl)
       setUrl(retreivedUrl)
 
-      // GlobalWorkerOptions.workerSrc =
-      //   '/chatbot/workers/pdf.worker.mjs';
+      GlobalWorkerOptions.workerSrc =
+        '/chatbot/workers/pdf.worker.mjs';
 
-      // const loadingTask = getDocument(retreivedUrl)
+      const loadingTask = getDocument(retreivedUrl)
 
-      // loadingTask.promise.then(pdf => {
-      //   pdf.getPage(1)
-      //   .then(page => {
-      //     const scale = 1.5
-      //     const viewport = page.getViewport({ scale })
-      //     const outputScale = window.devicePixelRatio || 1
+      loadingTask.promise.then(pdf => {
+        pdf.getPage(1)
+        .then(page => {
+          const scale = 1.5
+          const viewport = page.getViewport({ scale })
+          const outputScale = window.devicePixelRatio || 1
 
-      //     const canvas = document.getElementById('the-canvas') as HTMLCanvasElement
+          const canvas = document.getElementById('the-canvas') as HTMLCanvasElement
 
-      //     if (canvas) {
-      //       const context = canvas!.getContext("2d")
+          if (canvas) {
+            const context = canvas!.getContext("2d")
 
-      //       canvas.width = Math.floor(viewport.width * outputScale)
-      //       canvas.height = Math.floor(viewport.height * outputScale)
-      //       canvas.style.width = Math.floor(viewport.width) + "px"
-      //       canvas.style.height = Math.floor(viewport.height) + "px"
+            canvas.width = Math.floor(viewport.width * outputScale)
+            canvas.height = Math.floor(viewport.height * outputScale)
+            canvas.style.width = Math.floor(viewport.width) + "px"
+            canvas.style.height = Math.floor(viewport.height) + "px"
 
-      //       const transform = outputScale !== 1
-      //         ? [outputScale, 0, 0, outputScale, 0, 0]
-      //         : null;
+            const transform = outputScale !== 1
+              ? [outputScale, 0, 0, outputScale, 0, 0]
+              : null;
 
-      //       const renderContext = {
-      //         canvas,
-      //         canvasContext: context == null ? undefined : context,
-      //         transform: transform === null ? undefined : transform,
-      //         viewport,
-      //       }
+            const renderContext = {
+              canvas,
+              canvasContext: context == null ? undefined : context,
+              transform: transform === null ? undefined : transform,
+              viewport,
+            }
 
-      //       page.render(renderContext)
-      //     }
-      //   })
-      // })
+            page.render(renderContext)
+          }
+        })
+      })
     }
   }, [retreivedUrl, url])
 
-  // return (
-  //   <canvas id="the-canvas"></canvas>
-  // )
+  return (
+    <canvas id="the-canvas"></canvas>
+  )
 
   if (url === undefined) {
     return (
